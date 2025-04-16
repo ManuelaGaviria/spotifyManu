18d0502360fa89fdeb51383fba8a205d6d6604f4
ManuelaGaviria <manugaviria0507@gmail.com>
2025-04-16 11:51:30 -0500

    rutas, lazy loading, inicio del sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import(`./modules/home/home.module`).then(m => m.HomeModule)
+  }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
